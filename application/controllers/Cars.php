9b53adf3b710635044f5befa84e05160637304c2
Marcelo Santana <marcelo.scientist@hotmail.com>
2020-02-26 11:52:06 -0300

    The beginning of the basic system

--- application/controllers/Cars.php
+++ application/controllers/Cars.php
@@ -1,12 +1,12 @@
 <?php 
-    class Posts extends CI_Controller
+    class Cars extends CI_Controller
     {
         public function index()
         {
-            $data['title'] = 'Posts';
+            $data['title'] = 'Cars';
             
             $this->load->view('templates/header');
-            $this->load->view('posts/index', $data);
+            $this->load->view('cars/index', $data);
             $this->load->view('templates/footer');
 
         }
